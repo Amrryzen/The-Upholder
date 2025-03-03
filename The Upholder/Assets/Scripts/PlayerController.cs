46b7e4c8966083703dc6a70336c4f091ad36ddff
aeryoteob <mtobi.aryo@gmail.com>
2025-03-03 12:26:04 +0700

    Patch Temp 0.0.1
    Hanya penambahan basic fitur mekanik. (Prototype)

--- The Upholder/Assets/Scripts/PlayerController.cs
+++ The Upholder/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
 
+    Vector2 moveDirection;
+    Vector2 mousePosition;
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
